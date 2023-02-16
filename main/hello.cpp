8deb299b8bf6263c6ab0ffc6f5deb775ea6dff6d
soumyej102 <77912034+soumyej102@users.noreply.github.com>
2023-02-16 23:13:52 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World! I am Soumye Joshi!\n";
+  cout << "Hello, World! I am Soumye Joshi! I am from section F\n";
   cout << "Hello, Jenkins\n";
   cout << "I(PES2UG20CS346) have successfully built and run\n";
   return 0;
